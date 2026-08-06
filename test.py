38d16aca248a8f117e5a674dac68714ec3710e54
Anjana Girish <omganjana@gmail.com>
2026-08-06 14:15:14 +0530

    MCP with supervisor, guardrails and HITL

---
+++ test.py
@@ -0,0 +1,23 @@
+#from tools.tavily_tool import tavily_search
+#from tools.flight_tool import search_flights
+#from backend import run_travel_agent
+#res= tavily_search("Best hotels in India")
+
+#res = search_flights("Plan a 7 days Nepal trip from India")
+#print(res)
+
+#user_input =input("Enter travel request: ")
+
+#response = run_travel_agent(
+#    user_input=user_input,
+#    thread_id="test_user"
+#)
+
+#print("\nFINAL RESPONSE:\n")
+#print(response["answer"])
+
+import asyncio
+from mcp_client import get_all_tools,aviation_mcp_call
+
+if __name__ == "__main__":
+    asyncio.run(get_all_tools())
